c5db26b8412f34b3a0fe707155a2ad825cef5979
Shaughn Seepaul <sdseepaul@gmail.com>
2019-10-29 11:14:13 -0400

    Working search for customer portfolio

--- src/app/customer-portfolio/customer-portfolio.component.ts
+++ src/app/customer-portfolio/customer-portfolio.component.ts
@@ -19,4 +19,26 @@ export class CustomerPortfolioComponent implements OnInit {
   constructor() { }
   ngOnInit() {
   }
+
+  myFunction(){
+    // tslint:disable-next-line: one-variable-per-declaration
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    table = document.getElementById('port-table');
+    tr = table.getElementsByTagName('tr');
+
+  // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName('td')[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = '';
+        } else {
+          tr[i].style.display = 'none';
+        }
+      }
+    }
+  }
 }
