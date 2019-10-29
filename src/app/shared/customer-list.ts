b97b0e7e080afc0a5f2432e045a568012f595904
mziggy <mziggy@users.noreply.github.com>
2019-10-29 15:03:06 -0400

    Update customer-list.ts

--- src/app/shared/customer-list.ts
+++ src/app/shared/customer-list.ts
@@ -1,8 +1,8 @@
 import { Customer } from './customer.model';
 
 export const CUST: Customer[] = [
-  new Customer('1', '1', 'Molly'),
-  new Customer('2', '2', 'Dre'),
-  new Customer('3', '3', 'Shaughn'),
-  new Customer('4', '4', 'Bri')
+  new Customer('1', '1', '1', 'Molly'),
+  new Customer('2', '1', '2', 'Dre'),
+  new Customer('3', '2', '3', 'Shaughn'),
+  new Customer('4', '2', '4', 'Bri')
 ];
