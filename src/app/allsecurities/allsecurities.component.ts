8ff832f284a3b6a04a46d55fa1918147445db7b3
mziggy <mziggy@users.noreply.github.com>
2019-10-31 11:53:58 -0400

    update searching

--- src/app/allsecurities/allsecurities.component.ts
+++ src/app/allsecurities/allsecurities.component.ts
@@ -62,15 +62,28 @@ export class CustomerPortfolioComponent implements OnInit {
     tr = table.getElementsByTagName('tr');
 
   // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
+  for (i = 0; i < tr.length; i++) {
+      exists = false;
       td = tr[i].getElementsByTagName('td')[0];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = '';
-        } else {
-          tr[i].style.display = 'none';
+          exists = true;
         }
+      }
+      td = tr[i].getElementsByTagName('td')[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          exists = true;
+        }
+      }
+      console.log(tr[i].className);
+      if (exists || tr[i].className === 'header') {
+        // search exists in table and the element isn't the header
+        tr[i].style.display = '';
+      } else {
+        tr[i].style.display = 'none';
       }
     }
   }
